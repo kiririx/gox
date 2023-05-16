0cdb04d188c57fa404acff13d02836d7fbee67be
キリリ <kiriri_ri@126.com>
2023-05-16 17:20:59 +0800

    refactor: delete unused metadata

--- cmd/root.go
+++ cmd/root.go
@@ -14,9 +14,6 @@ var rootCmd = &cobra.Command{
 	Use: "gox",
 }
 
-var lib = ""
-var act = ""
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
