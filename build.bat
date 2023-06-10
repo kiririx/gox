b7bf8ad002c54e253a6c70eabc7bf27e6fe10ae0
kiririx <kiriri_ri@126.com>
2023-06-10 09:26:34 +0800

    feat: add script for windows

---
+++ build.bat
@@ -0,0 +1,30 @@
+@echo off
+:: 检查是否以管理员权限运行脚本
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo 请求管理员权限...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+
+go mod tidy
+go build
+
+move ".\gox.exe" "C:\Windows\System32"
+
+pause
